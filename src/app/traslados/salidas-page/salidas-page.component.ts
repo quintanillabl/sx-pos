71801e43d9c6a64fb8ac0a30e8553a27ad41872f
Ruben Cancino <rubencancino6@gmail.com>
2018-04-04 17:46:08 -0600

    fix

--- src/app/traslados/salidas-page/salidas-page.component.ts
+++ src/app/traslados/salidas-page/salidas-page.component.ts
@@ -6,53 +6,53 @@ import * as _ from 'lodash';
 import { Traslado } from 'app/logistica/models/traslado';
 import { TrasladosService } from 'app/traslados/services/traslados.service';
 
-
 @Component({
   selector: 'sx-salidas-page',
   templateUrl: './salidas-page.component.html'
 })
 export class SalidasPageComponent implements OnInit {
-
   traslados$: Observable<any>;
   procesando = false;
-  term = ''
+  term = '';
   _pendientes = false;
-  
-  constructor(
-    private service: TrasladosService
-  ) {}
-  
+
+  constructor(private service: TrasladosService) {}
+
   ngOnInit() {
     this.load();
   }
-  
+
   load() {
     this.traslados$ = this.service
-    .list({tipo: 'TPS', term: this.term, pendientes: this.pendientes})
-    .finally( () => this.procesando = false)
-    .catch( err => this.handleError(err))
+      .list({ tipo: 'TPS', term: this.term, pendientes: this.pendientes })
+      .finally(() => (this.procesando = false))
+      .catch(err => this.handleError(err));
   }
 
   search(term: string) {
     this.term = term;
     this.load();
   }
-  
+
   print(tps: Traslado) {
     this.procesando = true;
-    this.service.print(tps)
-      .finally( () => this.procesando = false)
-      .subscribe(res => {
-        const blob = new Blob([res], {
-          type: 'application/pdf'
-        });
-        const fileURL = window.URL.createObjectURL(blob);
-        window.open(fileURL, '_blank');
-      }, error2 => console.error(error2));
+    this.service
+      .print(tps)
+      .finally(() => (this.procesando = false))
+      .subscribe(
+        res => {
+          const blob = new Blob([res], {
+            type: 'application/pdf'
+          });
+          const fileURL = window.URL.createObjectURL(blob);
+          window.open(fileURL, '_blank');
+        },
+        error2 => console.error(error2)
+      );
   }
 
   handleError(err) {
-    console.log(err)
+    console.log(err);
     return Observable.empty();
   }
 
@@ -62,7 +62,6 @@ export class SalidasPageComponent implements OnInit {
   }
 
   get pendientes() {
-    return this._pendientes
+    return this._pendientes;
   }
-
 }
